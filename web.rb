c5cd237bdd2158430510e80566beaac53598db53
Joe McLean <mclean.joe@gmail.com>
2014-03-04 00:19:57 -0500

    Major update. Amplify tablet page complete, refactored to nav partial.

--- web.rb
+++ web.rb
@@ -7,6 +7,15 @@ get '/' do
   slim :index
 end
 
+get '/amplify_tablet' do
+  slim :amplify
+end
+
+get '/amplify_case' do
+  slim :amplify_case
+end
+
+
 get '/portfolio' do
   slim :portfolio
 end
